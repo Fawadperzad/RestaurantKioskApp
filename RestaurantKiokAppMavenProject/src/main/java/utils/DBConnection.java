6921044233fa46f3bd1d305d0c0af2b46b9bd97f
fawad <fawad_perzad@yahoo.com>
2026-05-25 09:15:17 +0200

    i changed the .env file to localy

--- RestaurantKiokAppMavenProject/src/main/java/utils/DBConnection.java
+++ RestaurantKiokAppMavenProject/src/main/java/utils/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/restaurant_db";
-    private static final String USER = "root";
-    private static final String PASS = "My$$Sql316";
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASS = System.getenv("DB_PASS");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
